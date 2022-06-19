52958958d908cb98ff85a245083919ae731ea9f6
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-06-19 12:51:22 +0300

    Doc: superuser created and a neighborhood class model

---
+++ comTwitter/urls.py
@@ -0,0 +1,22 @@
+
+# from msilib.schema import ListView
+from django.views.generic import ListView
+from django.urls import path
+from django.urls import include
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+
+urlpatterns = [
+
+    path('', views.landing, name='landing'),
+
+
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
