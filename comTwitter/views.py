e186411071f94b417ceb980e13982add34e49d8c
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-06-19 19:11:25 +0300

    Feat: Post message and is seen feature

--- comTwitter/views.py
+++ comTwitter/views.py
@@ -14,3 +14,19 @@ class postListView(View):
 
         return render(request, 'landing.html', {'posts': posts, 'form': form})
 
+    def post(self, request, *args, **kwargs):
+        posts = Post.objects.all().order_by('-created_on')
+        form = PostForm(request.POST)
+
+
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.author = request.user
+            new_post.save()
+
+            context={
+                'posts': posts, 
+                'form': form,
+            }
+
+        return render(request, 'landing.html', context)
